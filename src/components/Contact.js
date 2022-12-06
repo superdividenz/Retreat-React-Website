ebde5e44d22c444cee103223692befb65b3b07b0
p driscoll <superdividenz@gmail.com>
2022-12-06 12:06:55 -0600

    commit
    12/6

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,70 @@
 
-import React from 'react'
-import './Contact.css'
+import React from 'react';
+import './Contact.css';
+import { Grid, TextField, Button, Card, CardContent, Typography } from '@material-ui/core';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
-const Contact = () => {
-    return (
-        <div className='contact' id='contact'>
-            <div className='container'>
-                <h2>Contact</h2>
-                <span className='line'></span>
-                <div className='content'>
-                    <div className='card'>
+const render = (status: Status) => {
+    return <h1>{status}</h1>;
+    };
 
-                    </div>
-                    <div className='card'>
+function Contact() {
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="Contact" id="contact"> 
+      <Grid container>
+          
+        <Grid item xs={12} md={6}>
+        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              Contact
+          </Typography> 
+            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+              Fill up the form and our team will get back to you within 24 hours.
+          </Typography> 
+            <form>
+              <Grid container spacing={1}>
+                <Grid xs={12} sm={6} item>
+                  <TextField placeholder="Enter first name" label="First Name" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid xs={12} sm={6} item>
+                  <TextField placeholder="Enter last name" label="Last Name" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField type="number" placeholder="Enter phone number" label="Phone" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+                </Grid>
+
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+        <CardContent>
+        <Typography gutterBottom variant="h5">
+              Contact
+            </Typography>
+        </CardContent>
+            {/* <Wrapper apiKey={"YOUR_API_KEY"} render={render}>
+            <YourComponent/>
+            </Wrapper> */}
+        </Card>
+        </Grid>
+        
+      </Grid>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,30 +1,19 @@
+import React from "react"
+import { ComposableMap, Geographies, Geography } from "react-simple-maps"
 
-import React from 'react'
-import './Test.css'
+const geoUrl =
+  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
 
-const Test = () => {
-    return (
-        <div className='test' id='test'>
-            <div className='container'>
-                <h2>Test</h2>
-                <span className='line'></span>
-                <div className='content'>
-                    <div className='card'>
-
-                        <p>It is not every day that you come across a passionate and trustworthy financial advisor. John Doe is true professional who does his work really well. Thanks John!</p>
-                        <p><span>Johnson.M.J.</span></p>
-                        <p>Director of "Financial Times"</p>
-                    </div>
-                    <div className='card'>
-
-                        <p>In just 2 very short meetings with John he managed to find the solution personally catered to my situation and expectations. Punctual, well informed and very reliable.</p>
-                        <p><span>Carol Harper</span></p>
-                        <p>Director at Risktec Solutions Ltd</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+export default function MapChart() {
+  return (
+    <ComposableMap>
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+    </ComposableMap>
+  )
 }
-
-export default Test
